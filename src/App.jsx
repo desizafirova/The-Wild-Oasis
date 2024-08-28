8c98ca10b19a7ad998bcf99c97d5f6d7f550c184
DesiZafirova <desszafirowa@gmail.com>
2024-08-28 11:16:21 +0300

    creating reusable Row component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
+import Heading from './ui/Heading';
+import Row from './ui/Row';
+import Input from './ui/Input';
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
+const StyledApp = styled.main`
+  padding: 20px;
 `;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div>
-        <H1>The Wild Oasis</H1>
-        <Button>Check in</Button>
-      </div>
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button>Check in</Button>
+              <Button>Check out</Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
     </>
   );
 }
