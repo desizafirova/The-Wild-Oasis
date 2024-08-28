5a47cdefa3b6e080f22d5f89dd0360bb93a6c2a1
DesiZafirova <desszafirowa@gmail.com>
2024-08-28 15:00:54 +0300

    creating supabase service

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://fkmabdznybtnqrkjzxwl.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrbWFiZHpueWJ0bnFya2p6eHdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4NDIzNTgsImV4cCI6MjA0MDQxODM1OH0.cinKpz6VTvlbyiwEA10bvVqb-miLv7H3gtnL2___yv0';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
