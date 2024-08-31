69585d18631ae20b98b0f072bbeb5780d4348cfd
DesiZafirova <desszafirowa@gmail.com>
2024-08-31 20:29:53 +0300

    detaching clicks outside the modal component implemented

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react';
+
+export function useOutsideClick(handler, listenCaputuring = true) {
+  const ref = useRef();
+
+  useEffect(
+    function () {
+      function handleClick(e) {
+        if (ref.current && !ref.current.contains(e.target)) close();
+
+        handler();
+      }
+
+      document.addEventListener('click', handleClick, listenCaputuring);
+
+      return () =>
+        document.removeEventListener('click', handleClick, listenCaputuring);
+    },
+    [handler, listenCaputuring]
+  );
+
+  return ref;
+}
