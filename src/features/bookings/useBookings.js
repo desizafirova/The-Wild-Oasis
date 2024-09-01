6033d8177f78821b739c42b02538b1f4b58ffe76
DesiZafirova <desszafirowa@gmail.com>
2024-09-01 22:39:39 +0300

    implemented filtering bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+  // : { field: 'totalPrice', value: 5000, method: 'gte' };
+  /*  If I need to have a multiple filters at the same time -> Instead of passing in just one object I need to pass an array of objects and in the apiBooking file I have to loop over that object and for each of them I should add a new query to the query variable */
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, bookings, error };
