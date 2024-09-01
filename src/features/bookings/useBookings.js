f7e3ba39202af2ca91378eb68fb83f0ef9b9a559
DesiZafirova <desszafirowa@gmail.com>
2024-09-01 23:30:32 +0300

    sorting bookings implemented

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -12,13 +12,19 @@ export function useBookings() {
       : { field: 'status', value: filterValue };
   // : { field: 'totalPrice', value: 5000, method: 'gte' };
   /*  If I need to have a multiple filters at the same time -> Instead of passing in just one object I need to pass an array of objects and in the apiBooking file I have to loop over that object and for each of them I should add a new query to the query variable */
+
+  //   Sort
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings', filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
